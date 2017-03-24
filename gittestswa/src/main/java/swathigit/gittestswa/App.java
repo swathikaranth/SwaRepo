59e6bc4b41fef629bf492da031c4c55ba5eb7643
Swathi <swathikaranth01@gmail.com>
2017-03-23 18:35:46 -0600

    swathi first commit

---
+++ gittestswa/src/main/java/swathigit/gittestswa/App.java
@@ -0,0 +1,13 @@
+package swathigit.gittestswa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
